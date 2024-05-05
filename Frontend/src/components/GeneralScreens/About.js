ba4ca9063689b27f1ecdb2563552a6b6f26683f4
abhijayrawat <abhijay1rawat@gmail.com>
2024-05-06 03:36:56 +0530

    added about for Home page

---
+++ Frontend/src/components/GeneralScreens/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about__background-image" />
+      <div className="about__content">
+        <h1 className="about__title">About Us</h1>
+        <p className="about__description">
+          This is the about section. We provide the best travel services to our
+          customers.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
