d72a163c445a6c099e495a778ba49297b8d44caf
aarav0012 <aaravjainjn@email.com>
2024-05-06 01:44:10 +0530

    ist commit

--- Frontend/src/components/GeneralScreens/Footer.js
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -3,8 +3,29 @@ import '../../Css/Footer.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className="foot">
             <div className="footer">
+                <div className="compney-brief">
+                    <div className="footer-section about">
+                        <h3 className="logo-text">Travel Tales</h3>
+                        <p>
+                            Tales that cover the entire World.
+                        </p>
+                    </div>
+
+                    <div className="footer-logo">
+                        
+                    </div>
+                </div>
+
+                <div className="footer-contacts">
+                    <h2>Social Media</h2>
+                    <br/>
+                    {/* Add your social media links here */}
+                </div>
+                <div className='footer-more'>
+
+                </div>
             </div>
             <div className="copyright">
                 <p className="copyright-blog">© 2022 Gilbert Hutapea. All Rights Reserved</p>
